0313561b9377eec5944bf572f72d968046809e8e
geoffreydelric <geoffrey.delric.external@airbus.com>
2022-04-07 17:47:38 +0200

    resolve conflict

--- src/app/modules/study-case/study-case-management/study-case-management.component.ts
+++ src/app/modules/study-case/study-case-management/study-case-management.component.ts
@@ -275,10 +275,6 @@ export class StudyCaseManagementComponent implements OnInit, OnDestroy {
   }
 
   loadStudy(study: Study) {
-<<<<<<< HEAD
-=======
-  
->>>>>>> develop
     this.handleUnsavedChanges((changeHandled) => {
       if (changeHandled) {
         // Check user was in an another study before this one and leave room
