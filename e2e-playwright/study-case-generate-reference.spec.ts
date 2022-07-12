042663b0d4865c4d09f37c67341792691c7fd960
Nicolas Peuzin <nicolas.peuzin.external@airbus.com>
2022-07-12 14:21:52 +0200

    [Test e2e] Add generate reference test

---
+++ e2e-playwright/study-case-generate-reference.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+import { baseStudyCaseGenerateReference } from './base/base-study-case-generate-reference';
+
+test('Generate reference', async ({ page }) => {
+
+    const studyName = 'usecase_ZEROe_grid_search'
+    const repositoryName = 'business_case.v3.sos_processes.business_case'
+
+    await baseStudyCaseGenerateReference(page, studyName, repositoryName);
+    
+});
