9223d3613eb59cc6df937aaced53c5c802fc4293
Nicolas Peuzin <nicolas.peuzin.external@airbus.com>
2022-07-18 10:14:10 +0200

    [Test e2e] Change repo for reference generation

--- e2e-playwright/study-case-generate-reference.spec.ts
+++ e2e-playwright/study-case-generate-reference.spec.ts
@@ -3,7 +3,7 @@ import { baseStudyCaseGenerateReference } from './base/base-study-case-generate-
 
 test('Generate reference', async ({ page }) => {
 
-    const studyName = 'usecase_ZEROe_grid_search';
+    const studyName = 'usecase_ZEROe_grid_search_sensitivity';
     const repositoryName = 'business_case.v3.sos_processes.business_case';
 
     await baseStudyCaseGenerateReference(page, studyName, repositoryName);
