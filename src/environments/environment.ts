7ad84b3bb1e7e816f301ad6fdf847fa45330cfd3
ggoyon <guillaume.goyon@capgemini.com>
2023-10-16 09:58:40 +0200

    Changed direct ontology url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
   API_DATA_URL: 'http://127.0.0.1:8001',
   API_MESSAGE_URL: 'http://127.0.0.1:8002',
   API_POST_PROCESSING_URL: 'http://127.0.0.1:8003',
-  API_ONTOLOGY_DIRECT_URL: 'https://sostrades.eu.airbus.corp:31664'
+  API_ONTOLOGY_DIRECT_URL: 'http://127.0.0.1:5555'
 };
 
 /*
