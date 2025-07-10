7830bc5b6bb865342d4e287195d756ebd6e5a15e
Marie-Audrey Gueylard <marie-audrey.gueylard@capgemini.com>
2025-07-10 16:29:49 +0200

    modify llm popup

--- src/app/modules/chat-bot-dialog/chat-bot-dialog.component.ts
+++ src/app/modules/chat-bot-dialog/chat-bot-dialog.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogRef, MatDialogConfig  } from '@angular/material/dialog';
 import { StudyChatBotDialogData } from 'src/app/models/dialog-data.model';
 import { AppDataService } from 'src/app/services/app-data/app-data.service';
 import { environment } from 'src/environments/environment';
@@ -9,20 +9,52 @@ import { environment } from 'src/environments/environment';
   templateUrl: './chat-bot-dialog.component.html',
   styleUrls: ['./chat-bot-dialog.component.scss']
 })
-export class ChatBotDialogComponent {
+export class ChatBotDialogComponent implements OnInit {
 
-  
+  public isMinimized:boolean;
+  public startPosition:any;
   constructor( 
     public dialogRef: MatDialogRef<ChatBotDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: StudyChatBotDialogData,
     ) 
-    { }
+    { 
+      this.isMinimized = false;
+    }
 
+  ngOnInit(): void {
+    this.dialogRef.afterOpened().subscribe(()=>{
+      this.startPosition = this.dialogRef.componentRef.location.nativeElement.getBoundingClientRect();
+    });
+  }
 
   closeDialog() {
     if (this.dialogRef !== null && this.dialogRef !== undefined) {
       this.dialogRef.close();
       this.dialogRef = null;
+    }
+  }
+
+  minimizeDialog() {
+    if (this.dialogRef !== null && this.dialogRef !== undefined) {
+      if (this.isMinimized){
+        const rect = this.dialogRef.componentRef.location.nativeElement.getBoundingClientRect();
+        this.dialogRef.updateSize("600px","800px");
+        const matDialogConfig = new MatDialogConfig();
+        matDialogConfig.position = {top:`${this.startPosition.top}px`, right:`0px`};
+        this.dialogRef.updatePosition(matDialogConfig.position);
+      
+        
+      }
+      else{
+        this.dialogRef.updateSize("600px","30px")
+        const matDialogConfig = new MatDialogConfig();
+        matDialogConfig.position = {top:`${this.startPosition.top + 800 -30}px`, right:`0px`};
+        this.dialogRef.updatePosition(matDialogConfig.position);
+      
+        
+      }
+      this.isMinimized = !this.isMinimized;
+      
     }
   }
 
--- src/app/modules/study-case/study-case-event-container/study-case-event-container.component.ts
+++ src/app/modules/study-case/study-case-event-container/study-case-event-container.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { ChatBotDialogComponent } from '../../chat-bot-dialog/chat-bot-dialog.component';
-import { MatDialog } from '@angular/material/dialog';
+import { MatDialog, MatDialogRef, MatDialogState } from '@angular/material/dialog';
 import { StudyChatBotDialogData } from 'src/app/models/dialog-data.model';
 import { StudyCaseDataService } from 'src/app/services/study-case/data/study-case-data.service';
 
@@ -12,12 +12,14 @@ import { StudyCaseDataService } from 'src/app/services/study-case/data/study-cas
 })
 export class StudyCaseEventContainerComponent implements OnInit, OnDestroy{
   public showChatBotButton: boolean;
+  private chatBotDialog: MatDialogRef<ChatBotDialogComponent>;
 
   constructor(
     public dialog: MatDialog,
     private studyCaseDataService: StudyCaseDataService
   ){
     this.showChatBotButton = false;
+    this.chatBotDialog = null;
   }
 
   ngOnInit(): void {
@@ -30,23 +32,29 @@ export class StudyCaseEventContainerComponent implements OnInit, OnDestroy{
   }
 
   openChatBot(){
-
-    const studyId = this.studyCaseDataService.loadedStudy.studyCase.id;
-    let chatbotData = new StudyChatBotDialogData();
-    chatbotData.studyName = this.studyCaseDataService.loadedStudy.studyCase.name;
-    chatbotData.chatBotUrl = environment['STUDY_LLM_URL']  + '/?embed=true&study-id=' + studyId ;
-    this.dialog.open(ChatBotDialogComponent, {
-      width: '400px', 
-      position: { right: '0'},
-      height: '600px',
-      data: chatbotData,
-      hasBackdrop: false
-    });
+    if ((this.chatBotDialog !== null && this.chatBotDialog !== undefined && this.chatBotDialog.getState() !== MatDialogState.OPEN)
+    || this.chatBotDialog === null || this.chatBotDialog === undefined){
+      const studyId = this.studyCaseDataService.loadedStudy.studyCase.id;
+      let chatbotData = new StudyChatBotDialogData();
+      chatbotData.studyName = this.studyCaseDataService.loadedStudy.studyCase.name;
+      chatbotData.chatBotUrl = environment['STUDY_LLM_URL']  + '/?embed=true&study-id=' + studyId ;
+      this.chatBotDialog = this.dialog.open(ChatBotDialogComponent, {
+        width: '600px', 
+        position: { right: '0', bottom:'0'},
+        height: '800px',
+        data: chatbotData,
+        hasBackdrop: false
+      });
+    }
+    else{
+      this.chatBotDialog.close();
+    }
   }
 
   ngOnDestroy(): void {
     if (this.dialog !== null && this.dialog !== undefined) {
       this.dialog.closeAll();
+      this.chatBotDialog = null;
     }
   }
 
