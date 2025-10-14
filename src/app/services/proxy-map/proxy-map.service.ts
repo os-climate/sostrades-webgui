a5604f408d1508b9d3c337f4be0377510d1333ae
Marie-Audrey Gueylard <marie-audrey.gueylard@capgemini.com>
2025-10-14 10:22:11 +0200

    change proxy-map url

--- src/app/services/proxy-map/proxy-map.service.ts
+++ src/app/services/proxy-map/proxy-map.service.ts
@@ -1,18 +1,22 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Location } from '@angular/common';
-import { DataHttpService } from '../http/data-http/data-http.service';
+import { BaseHttpService } from '../http/base-http/base-http.service';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ProxyMapService extends DataHttpService {
+export class ProxyMapService extends BaseHttpService {
 
   constructor(
-    private http: HttpClient,
-    private location: Location) {
-      super(location, 'proxy-map');
-   }
-
+      location: Location,
+      private serviceName: string) {
+        super(location, 'proxy-map', '');
+    }
+  
+    public get apiRoute(): string {
+      return `${super.apiRoute}/${this.serviceName}`;
+    }
   
 }
